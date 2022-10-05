bee5ee04afd6d65aa1790e3fb9a9b2593ef04bbd
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2022-10-05 07:49:30 +0300

    shop

---
+++ public/js/card.js
@@ -0,0 +1,22 @@
+let cart = {};
+document.querySelectorAll('.add-to-cart').forEach(function (element) {
+    element.onclick = addToCart;
+});
+
+function addToCart() {
+    let goodsId = this.dataset.goods_id;
+    if (cart[goodsId]) {
+        cart[goodsId]++;
+    } else {
+        cart[goodsId] = 1;
+    }
+    console.log(cart);
+}
+
+
+function ajaxGetGoodsInfo() {
+    fetch('/get-goods-info', {
+        method: 'POST',
+        body: JSON.stringify({ key: Object.keys(cart) }),
+    });
+}
